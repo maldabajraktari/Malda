c270329f00d0f28f977287025ea8f081132739bc
Malda <maldaab2007@gmail.com>
2024-10-18 17:01:06 +0200

    added variable lists

---
+++ Module 1/main.py
@@ -0,0 +1,16 @@
+emri = "Malda"
+mosha = 17
+temperatura = 25.5
+
+print(emri)
+
+print(type(emri))
+print(type(mosha))
+print(type(temperatura))
+
+mosha = mosha+10
+
+my_data=["malda","bajraktari",17,"Shkolla Digjitale"]
+mosha=my_data
+
+my_data.append("Prishtina")
